43310b60a8304c8e0397eec4397ce46877cb348b
trungnguyen <quytrung2406@gmail.com>
2026-05-15 15:40:27 +0700

    set up Tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div>
-      <h1>BlueMoon AMS</h1>
-      <p>Apartment Management System</p>
+      <h1 className="text-3xl font-bold">BlueMoon AMS</h1>
+      <p className="text-lg">Apartment Management System</p>
     </div>
   );
 }
