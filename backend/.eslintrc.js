94c60a29fe81bfd2a423483a908cb363b0ab0044
khacduy138 <66925562+khacduy138@users.noreply.github.com>
2026-06-07 10:59:23 +0700

    fix(m4): wrap createPayment in transaction, fix association aliases; add ESLint config; clean server.js

---
+++ backend/.eslintrc.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
